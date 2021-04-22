b9d25be12ac5bc500a8343252b40b733c8a51c51
magnuhla <magnuhla@ntnu.no>
2021-04-22 18:39:11 +0200

    Fixed dif function

--- simworld.py
+++ simworld.py
@@ -35,8 +35,8 @@ class SimWorld:
 
         finished = self.is_finished(position, velocity)
         # Remove all but line 43(last reward define) to get back to normal rewards
-        dif = 0.5*(velocity**2-self.velocity**2) + 9.81 * \
-            (math.cos(3*((position-self.position)+math.pi/2)))
+        dif = 0.5 * (velocity ** 2 - self.velocity ** 2) + 9.81 * (
+                    math.cos(3 * (position + math.pi / 2)) - math.cos(3 * (self.position + math.pi / 2)))
         if dif > 0:
             reward = 1 if self.is_finished(position, velocity) else 0
         else:
