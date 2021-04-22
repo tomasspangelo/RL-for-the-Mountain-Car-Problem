0c8613d83f95df6957564639e8d0197ec01b141e
magnuhla <magnuhla@ntnu.no>
2021-04-22 13:22:34 +0200

    Removed simworld from actor

--- actor.py
+++ actor.py
@@ -6,18 +6,18 @@ class Actor:
     Class for an actor.
     """
 
-    def __init__(self, keras_model, simworld, epsilon, epsilon_decay):
+    def __init__(self, keras_model, epsilon, epsilon_decay):
         self.policy = keras_model
-        self.simworld = simworld
         self.epsilon = epsilon
         self.epsilon_decay = epsilon_decay
 
     def get_q(self, tiled_state, action):
         state_action_pair = tiled_state.copy().append(action)
+        state_action_pair.append(action)
         return self.policy(state_action_pair)
 
     def get_action(self, tiled_state):
-        legal_actions = [-1, 0, 1]  # Can change to simworld.get_legal_actions()
+        legal_actions = [-1, 0, 1]
         if random.random() < self.epsilon:
             return random.choice(legal_actions)
         best_action = None
@@ -29,12 +29,9 @@ class Actor:
                 best_action = a
         return best_action
 
-    # Needed?
-    def perform_action(self, action):
-        return self.simworld.perform_action(action)
-
     def update_policy(self, tiled_state, action, target):
-        state_action_pair = tiled_state.copy().append(action)
+        state_action_pair = tiled_state.copy()
+        state_action_pair.append(action)
         self.policy.fit(state_action_pair, target, epochs=1, verbosity=0)
 
     def update_epsilon(self):
