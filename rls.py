6d03421ea698f0a6d6b7744e4e6eaa51d1fd270c
sigursl <sigursl@stud.ntnu.no>
2021-04-23 15:02:23 +0200

    Revert "add timestamp to data file"
    This reverts commit d760c87d8213eaceb09852be5e04f731a261d227.

--- rls.py
+++ rls.py
@@ -5,7 +5,6 @@ import time
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
-from datetime import datetime
 
 
 class ReinforcementLearningSystem:
@@ -80,7 +79,7 @@ class ReinforcementLearningSystem:
 
         data = pd.DataFrame({"Steps": progress})
         data["Episodes"] = [i for i in range(self.episodes)]
-        timestamp = datetime.now().strftime("%m_%d_%H:%M:%S")
-        data.to_pickle(f"data/data_{timestamp}")
+        file = open("./data/data.pickle", "wb")
+        pickle.dump(data, file)
         data.plot.scatter(x="Episodes", y="Steps")
         plt.show()
